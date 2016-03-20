ff4b4b52834dae2f67db66f0892ba2d3c89af3c9
fen555 <cofen.yensira@gmail.com>
2016-03-20 18:53:58 +0700

    edit standard

--- js/index.js
+++ js/index.js
@@ -25,7 +25,13 @@ shoppingList.controller('index', ['$scope', function ($scope) {
 
   $scope.getDiscount = function (total) {
     var discount = 0
-    var num, num2, num3, num4, num5, num6, num7 = 0
+    var num = 0
+    var num2 = 0
+    var num3 = 0
+    var num4 = 0
+    var num5 = 0
+    var num6 = 0
+    var num7 = 0
     for (var i = 0; i < $scope.listBook.length; i++) {
       if ($scope.listBook[i] === 1) {
         num += 1
@@ -68,49 +74,49 @@ shoppingList.controller('index', ['$scope', function ($scope) {
   $scope.addItem = function () {
     if ($scope.itemQuantity1 !== 0) {
       $scope.list.push({name: "Harry Potter and the Philosopher's Stone", shop: '1', quantity: $scope.itemQuantity1, price: 100 * $scope.itemQuantity1})
-      for ($n = 0 ; $n < $scope.itemQuantity1 ; $n++) {
+      for ($n = 0; $n < $scope.itemQuantity1; $n++) {
         $scope.listBook.push(1)
       }
       $scope.itemQuantity1 = 0
     }
     if ($scope.itemQuantity2 !== 0) {
       $scope.list.push({name: 'Harry Potter and the Chamber of Secrets', shop: '2', quantity: $scope.itemQuantity2, price: 100 * $scope.itemQuantity2})
-      for ($n = 0 ; $n < $scope.itemQuantity2 ; $n++) {
+      for ($n = 0; $n < $scope.itemQuantity2; $n++) {
         $scope.listBook.push(2)
       }
       $scope.itemQuantity2 = 0
     }
     if ($scope.itemQuantity3 !== 0) {
       $scope.list.push({name: 'Harry Potter and the Prisoner of Azkaban', shop: '3', quantity: $scope.itemQuantity3, price: 100 * $scope.itemQuantity3})
-      for ($n = 0 ; $n < $scope.itemQuantity3 ; $n++) {
+      for ($n = 0; $n < $scope.itemQuantity3; $n++) {
         $scope.listBook.push(3)
       }
       $scope.itemQuantity3 = 0
     }
     if ($scope.itemQuantity4 !== 0) {
       $scope.list.push({name: 'Harry Potter and the Goblet of Fire', shop: '4', quantity: $scope.itemQuantity4, price: 100 * $scope.itemQuantity4})
-      for ($n = 0 ; $n < $scope.itemQuantity4 ; $n++) {
+      for ($n = 0; $n < $scope.itemQuantity4; $n++) {
         $scope.listBook.push(4)
       }
       $scope.itemQuantity4 = 0
     }
     if ($scope.itemQuantity5 !== 0) {
       $scope.list.push({name: 'Harry Potter and the Order of the Phoenix', shop: '5', quantity: $scope.itemQuantity5, price: 100 * $scope.itemQuantity5})
-      for ($n = 0 ; $n < $scope.itemQuantity5 ; $n++) {
+      for ($n = 0; $n < $scope.itemQuantity5; $n++) {
         $scope.listBook.push(5)
       }
       $scope.itemQuantity5 = 0
     }
     if ($scope.itemQuantity6 !== 0) {
       $scope.list.push({name: 'Harry Potter and the Half-Blood Prince', shop: '6', quantity: $scope.itemQuantity6, price: 100 * $scope.itemQuantity6})
-      for ($n = 0 ; $n < $scope.itemQuantity6 ; $n++) {
+      for ($n = 0; $n < $scope.itemQuantity6; $n++) {
         $scope.listBook.push(6)
       }
       $scope.itemQuantity6 = 0
     }
     if ($scope.itemQuantity7 !== 0) {
       $scope.list.push({name: 'Harry Potter and the Deathly Hallows', shop: '7', quantity: $scope.itemQuantity7, price: 100 * $scope.itemQuantity7})
-      for ($n = 0 ; $n < $scope.itemQuantity7 ; $n++) {
+      for ($n = 0; $n < $scope.itemQuantity7; $n++) {
         $scope.listBook.push(7)
       }
       $scope.itemQuantity7 = 0
