66d67240a62110ce20fb9f545ce177d714624144
fen555 <cofen.yensira@gmail.com>
2016-03-20 14:11:05 +0700

    3rd

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 /* global angular */
 var shoppingList = angular.module('root', [])
 shoppingList.controller('index', ['$scope', function ($scope) {
-  $scope.itemName
-  $scope.itemShop
-  $scope.itemQuantity
-  $scope.itemPrice
+  $scope.itemQuantity1 = 0
+  $scope.itemQuantity2 = 0
+  $scope.itemQuantity3 = 0
+  $scope.itemQuantity4 = 0
+  $scope.itemQuantity5 = 0
+  $scope.itemQuantity6 = 0
+  $scope.itemQuantity7 = 0
 
   $scope.list = [
     {name: "Harry Potter and the Philosopher's Stone", shop: '1', quantity: 1, price: 100}
 
   ]
-
   $scope.getTotal = function () {
     var total = 0
     for (var i = 0; i < $scope.list.length; i++) {
@@ -29,12 +31,34 @@ shoppingList.controller('index', ['$scope', function ($scope) {
     list.splice(0, length)
   }
 
-  $scope.addItem = function (book) {
-    if (book === 1) {
+  $scope.addItem = function () {
+    if ($scope.itemQuantity1 !== 0) {
       $scope.list.push({name: "Harry Potter and the Philosopher's Stone", shop: '1', quantity: $scope.itemQuantity1, price: 100 * $scope.itemQuantity1})
       $scope.itemQuantity1 = 0
-    } else {
-      $scope.list.push({name: $scope.itemName, shop: $scope.itemShop, quantity: $scope.itemQuantity, price: 100 * $scope.itemQuantity, checked: false})
+    }
+    if ($scope.itemQuantity2 !== 0) {
+      $scope.list.push({name: 'Harry Potter and the Chamber of Secrets', shop: '2', quantity: $scope.itemQuantity2, price: 100 * $scope.itemQuantity2})
+      $scope.itemQuantity2 = 0
+    }
+    if ($scope.itemQuantity3 !== 0) {
+      $scope.list.push({name: 'Harry Potter and the Prisoner of Azkaban', shop: '3', quantity: $scope.itemQuantity3, price: 100 * $scope.itemQuantity3})
+      $scope.itemQuantity3 = 0
+    }
+    if ($scope.itemQuantity4 !== 0) {
+      $scope.list.push({name: 'Harry Potter and the Goblet of Fire', shop: '4', quantity: $scope.itemQuantity4, price: 100 * $scope.itemQuantity4})
+      $scope.itemQuantity4 = 0
+    }
+    if ($scope.itemQuantity5 !== 0) {
+      $scope.list.push({name: 'Harry Potter and the Order of the Phoenix', shop: '5', quantity: $scope.itemQuantity5, price: 100 * $scope.itemQuantity5})
+      $scope.itemQuantity5 = 0
+    }
+    if ($scope.itemQuantity6 !== 0) {
+      $scope.list.push({name: 'Harry Potter and the Half-Blood Prince', shop: '6', quantity: $scope.itemQuantity6, price: 100 * $scope.itemQuantity6})
+      $scope.itemQuantity6 = 0
+    }
+    if ($scope.itemQuantity7 !== 0) {
+      $scope.list.push({name: 'Harry Potter and the Deathly Hallows', shop: '7', quantity: $scope.itemQuantity7, price: 100 * $scope.itemQuantity7})
+      $scope.itemQuantity7 = 0
     }
   }
 }])
