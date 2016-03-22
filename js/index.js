278b8bfa13ebdcf1b6f024dcc363c4a52146a390
fen555 <cofen.yensira@gmail.com>
2016-03-23 01:57:31 +0700

    before sleep

--- js/index.js
+++ js/index.js
@@ -1,17 +1,8 @@
 /* global angular */
 var eppingList = angular.module('root', [])
 eppingList.controller('index', ['$scope', function ($scope) {
-  $scope.book = [
-    {name: "Harry Potter and the Philosopher's Stone", ep: 1, price: 100, img: 'img/1.jpeg'},
-    {name: 'Harry Potter and the Chamber of Secrets', ep: 2, price: 100, img: 'img/2.jpeg'},
-    {name: 'Harry Potter and the Prisoner of Azkaban', ep: 3, price: 100, img: 'img/3.jpeg'},
-    {name: 'Harry Potter and the Goblet of Fire', ep: 4, price: 100, img: 'img/4.jpeg'},
-    {name: 'Harry Potter and the Order of the Phoenix', ep: 5, price: 100, img: 'img/5.jpeg'},
-    {name: 'Harry Potter and the Half-Blood Prince', ep: 6, price: 100, img: 'img/6.jpeg'},
-    {name: 'Harry Potter and the Deathly Hallows', ep: 7, price: 100, img: 'img/7.jpeg'}
-  ]
   $scope.list = []
-  $scope.list2 = []
+  // $scope.list2 = []
   $scope.getTotal = function () {
     var total = 0
     for (var i = 0; i < $scope.list.length; i++) {
@@ -19,22 +10,23 @@ eppingList.controller('index', ['$scope', function ($scope) {
     }
     return total
   }
-  $scope.getDiscount = function (list) {
-  }
 
   $scope.remove = function (item) {
     var index = $scope.list.indexOf(item)
     $scope.list.splice(index, 1)
+    // $scope.list2.splice(index, 1)
   }
 
   $scope.clearAll = function (list) {
     var length = list.length
     list.splice(0, length)
+    // list2.splice(0, length)
   }
   $scope.addItem = function (ep, name) {
     if (check($scope.list, ep)) {
       var index = findArrBook($scope.list, ep)
       $scope.list[index].amount += 1
+      // $scope.list2[index].amount += 1
     } else {
       var data = {
         ep: ep,
@@ -42,9 +34,19 @@ eppingList.controller('index', ['$scope', function ($scope) {
         amount: 1
       }
       $scope.list.push(data)
-      console.log($scope.list)
-      $scope.list2 = $scope.list
     }
+
+    $scope.list.sort(function (a, b) {
+      if (a.amount > b.amount) {
+        return -1
+      }
+      if (a.amount < b.amount) {
+        return 1
+      }
+      return 0
+    })
+
+    $scope.getDiscount()
   }
   var check = function (list, ep) {
     for (var i = 0; i < list.length; i++) {
@@ -59,5 +61,66 @@ eppingList.controller('index', ['$scope', function ($scope) {
         return i
       }
     }
+  }
+  $scope.getDiscount = function () {
+    // var listss = $scope.list2
+    // // console.log(listss)
+    // var ee = 0
+    // var num = 0
+    // for (var i = ee; i < listss.length; i++) {
+    //   num += listss[i].amount
+    //   ee++
+    // }
+    // // ///////////////////// /
+    // var countlist = 0 // เช็ครอบเข้า เงื่อนไขลดราคา
+    // var sell = 0 // ส่วนลด
+    // var exit = 0
+    // do {
+    //   // /////// for เช็ค จำนวนเล่มหนังสือ
+    //   for (var i = 0; i < listss.length; i++) {
+    //     if (listss[i].amount !== 0) {
+    //       countlist += 1
+    //     }
+    //     if (listss[i].amount === 0) {
+    //       countlist += 0
+    //       break
+    //     }
+    //   }
+    //   // ///// เงื่อนไขลดราคา
+    //   if (countlist === 1) {
+    //     // มีไว้เฉยๆๆ ไม่มีทำงานไม่ได้
+    //     console.log('1')
+    //   } if (countlist === 2) {
+    //     sell += ((countlist * 100) * 0.1)
+    //     console.log('2')
+    //   } else if (countlist === 3) {
+    //     sell += ((countlist * 100) * 0.2)
+    //     console.log('3')
+    //   } else if (countlist === 4) {
+    //     sell += ((countlist * 100) * 0.3)
+    //     console.log('4')
+    //   } else if (countlist === 5) {
+    //     sell += ((countlist * 100) * 0.4)
+    //     console.log('5')
+    //   } else if (countlist === 6) {
+    //     sell += ((countlist * 100) * 0.5)
+    //     console.log('6')
+    //   } else if (countlist === 7) {
+    //     sell += ((countlist * 100) * 0.6)
+    //     console.log('7')
+    //   } else if (countlist === 0) {
+    //     exit = 1
+    //     console.log('exit')
+    //   }
+    //   console.log(sell)
+    //   // // ลบ จำนวนออกทีละ 1
+    //   for (var i = 0; i < listss.length; i++) {
+    //     if (listss[i].amount > 0) {
+    //       listss[i].amount -= 1
+    //       countlist = 0 // รีค่าใหม่
+    //     }
+    //   }
+    // } while (exit !== 1)
+    // return sell
   }
 }])
