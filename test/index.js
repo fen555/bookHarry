6d90bdc9faf8a3d2cef95da551b48ed86426fc6b
fen555 <cofen.yensira@gmail.com>
2016-03-27 12:22:01 +0700

    edit

--- test/index.js
+++ test/index.js
@@ -25,9 +25,34 @@ describe('TEST Buy Harry Potter Book', function () {
     afterEach(function * () {
       yield nightmare.end()
     })
+    it('ซื้อเล่ม 1 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 0', function * () {
+      var case1 = yield nightmare
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
+        .wait(2000)
+        .click('.book1')
+        .evaluate(function () {
+          this.price = document.querySelector('.total2').innerHTML
+          return this.price.substr(1, this.price.length - 1)
+        })
+      case1.should.equal('0.00')
+    })
+    it('ซื้อเล่ม 2 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 0', function * () {
+      var case1 = yield nightmare
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
+        .wait(2000)
+        .click('.book2')
+        .evaluate(function () {
+          this.price = document.querySelector('.total2').innerHTML
+          return this.price.substr(1, this.price.length - 1)
+        })
+      case1.should.equal('0.00')
+    })
     it('ซื้อเล่ม 1 จำนวน 2 เล่ม ส่วนลดต้องเท่ากับ 0', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book1')
@@ -39,7 +64,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อเล่ม 1 จำนวน 2 เล่ม, ซื้อเล่ม 2 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 20', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book1')
@@ -53,7 +79,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อเล่ม 1 จำนวน 2 เล่ม, ซื้อเล่ม 2 จำนวน 2 เล่ม ส่วนลดต้องเท่ากับ 20', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book1')
@@ -68,7 +95,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อเล่ม 3 จำนวน 3 เล่ม, ซื้อเล่ม 4 จำนวน 3 เล่ม ส่วนลดต้องเท่ากับ 60', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book3')
         .click('.book3')
@@ -85,7 +113,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อเล่ม 1 จำนวน 1 เล่ม, ซื้อเล่ม 2 จำนวน 1 เล่ม, ซื้อเล่ม 4 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 60', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book2')
@@ -99,7 +128,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อเล่ม 1 จำนวน 10 เล่ม ส่วนลดต้องเท่ากับ 0', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book1')
@@ -120,7 +150,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อเล่ม 1-3 ภาคละ 1 เล่ม ส่วนลดต้องเท่ากับ 60', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book2')
@@ -134,7 +165,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อเล่ม 1-4 ภาคละ 1 เล่ม ส่วนลดต้องเท่ากับ 120', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book2')
@@ -149,7 +181,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อเล่ม 1-5 ภาคละ 1 เล่ม ส่วนลดต้องเท่ากับ 200', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book2')
@@ -165,7 +198,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อเล่ม 1-6 ภาคละ 1 เล่ม ส่วนลดต้องเท่ากับ 300', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book2')
@@ -182,7 +216,8 @@ describe('TEST Buy Harry Potter Book', function () {
     })
     it('ซื้อทุกเล่ม ภาคละ 1 เล่ม ส่วนลดต้องเท่ากับ 420', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book2')
@@ -198,9 +233,64 @@ describe('TEST Buy Harry Potter Book', function () {
         })
       case1.should.equal('420.00')
     })
+    it('ซื้อเล่ม 3 จำนวน 6 เล่ม, ซื้อเล่ม 4 จำนวน 6 เล่ม ส่วนลดต้องเท่ากับ 120', function * () {
+      var case1 = yield nightmare
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
+        .wait(2000)
+        .click('.book3')
+        .click('.book3')
+        .click('.book3')
+        .click('.book3')
+        .click('.book3')
+        .click('.book3')
+        .click('.book4')
+        .click('.book4')
+        .click('.book4')
+        .click('.book4')
+        .click('.book4')
+        .click('.book4')
+        .wait(1000)
+        .evaluate(function () {
+          this.price = document.querySelector('.total2').innerHTML
+          return this.price.substr(1, this.price.length - 1)
+        })
+      case1.should.equal('120.00')
+    })
+    it('ซื้อเล่ม 3 จำนวน 6 เล่ม, ซื้อเล่ม 4 จำนวน 6 เล่ม, ซื้อเล่ม 5 จำนวน 6 เล่ม ส่วนลดต้องเท่ากับ 360', function * () {
+      var case1 = yield nightmare
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
+        .wait(2000)
+        .click('.book3')
+        .click('.book3')
+        .click('.book3')
+        .click('.book3')
+        .click('.book3')
+        .click('.book3')
+        .click('.book4')
+        .click('.book4')
+        .click('.book4')
+        .click('.book4')
+        .click('.book4')
+        .click('.book4')
+        .click('.book5')
+        .click('.book5')
+        .click('.book5')
+        .click('.book5')
+        .click('.book5')
+        .click('.book5')
+        .wait(1000)
+        .evaluate(function () {
+          this.price = document.querySelector('.total2').innerHTML
+          return this.price.substr(1, this.price.length - 1)
+        })
+      case1.should.equal('360.00')
+    })
     it('ซื้อเล่ม 1 จำนวน 6 เล่ม, ซื้อเล่ม 2 จำนวน 5 เล่ม, ซื้อเล่ม 3 จำนวน 4 เล่ม, ซื้อเล่ม 4 จำนวน 1 เล่ม ส่วนลดต้องเท่ากับ 320', function * () {
       var case1 = yield nightmare
-        .goto('http://cofen.herokuapp.com/')
+        .goto('http://localhost:5000')
+        // .goto('http://cofen.herokuapp.com/')
         .wait(2000)
         .click('.book1')
         .click('.book1')
